9f4b837fbb7ccaf256aec2cd649ca7b6a2b583eb
Hassan <mughalhassan467@gmail.com>
2019-02-12 18:08:31 +0500

    HM

--- app/src/main/java/com/itpvt/mediaplayer/MainActivity.java
+++ app/src/main/java/com/itpvt/mediaplayer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.itpvt.mediaplayer;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -32,10 +37,28 @@ arraycycle= new ArrayList<>();
 
 fetchVideos();
 
+        int Permission_All = 1;
+
+        String[] Permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.WRITE_SETTINGS};
+        if(!hasPermissions(this, Permissions)){
+            ActivityCompat.requestPermissions(this, Permissions, Permission_All);
+        }
 
 
 
+    }
 
+
+    public static boolean hasPermissions(Context context, String... permissions){
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M && context!=null && permissions!=null){
+            for(String permission: permissions){
+                if(ActivityCompat.checkSelfPermission(context, permission)!= PackageManager.PERMISSION_GRANTED){
+                    return  false;
+                }
+            }
+        }
+        return true;
     }
 
     private void fetchVideos() {
