99dbf4c726be329f1b56eda076582b9d1362eca1
Hassan <mughalhassan467@gmail.com>
2019-02-12 17:42:11 +0500

    HM

---
+++ app/src/main/java/com/itpvt/mediaplayer/Permission.java
@@ -0,0 +1,70 @@
+package com.itpvt.mediaplayer;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Permission extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                requestPermission();
+            }
+        }, 2000);
+    }
+
+
+    void requestPermission() {
+        if (ContextCompat.checkSelfPermission(Permission.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(Permission.this, "You have already granted this permission!",
+                    Toast.LENGTH_SHORT).show();
+
+            Intent in = new Intent(Permission.this, MainActivity.class);
+            startActivity(in);
+            finish();
+        }
+
+
+        else
+
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 10);
+        }
+    }
+
+}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 10);
+            }
+        }else {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+}
